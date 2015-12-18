7b178ec1f1250c38c8ff09346744faa00d07e855
maxmfernan <maxmfernan@gmail.com>
2015-12-18 00:38:52 -0200

    arquivos adiconados

---
+++ java/io/PrintStream.java
@@ -0,0 +1,75 @@
+package java.io;
+
+public class PrintStream {
+	public void print(char c) {
+
+	}
+
+	public void print(double d) {
+
+	}
+
+	public void print(float f) {
+
+	}
+
+	public void print(int f) {
+
+	}
+
+	public void print(long f) {
+
+	}
+
+	public void print(Object f) {
+
+	}
+
+	public void print(String s) {
+
+	}
+
+	public void print(boolean b) {
+
+	}
+
+	public void print(char[] carr) {
+
+	} 
+
+	public void println(char c) {
+
+	}
+
+	public void println(double d) {
+
+	}
+
+	public void println(float f) {
+
+	}
+
+	public void println(int f) {
+
+	}
+
+	public void println(long f) {
+
+	}
+
+	public void println(Object f) {
+
+	}
+
+	public void println(String s) {
+
+	}
+
+	public void println(boolean b) {
+
+	}
+
+	public void println(char[] carr) {
+		
+	} 
+}
